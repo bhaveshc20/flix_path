7126028a1929128ee2e5ec452ce057dcb31d93a2
bhaveshc20 <bhaveshc789@gmail.com>
2018-01-11 12:13:02 +0530

    Initial Commit

---
+++ flix_pathTests/flix_pathTests.swift
@@ -0,0 +1,36 @@
+//
+//  flix_pathTests.swift
+//  flix_pathTests
+//
+//  Created by Bhavesh on 21/10/1939 Saka.
+//  Copyright © 1939 Bhavesh. All rights reserved.
+//
+
+import XCTest
+@testable import flix_path
+
+class flix_pathTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
