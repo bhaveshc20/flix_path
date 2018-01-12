4860db6d9ffd8f5e36fbf814856827f35f6a3709
bhaveshc20 <bhaveshc789@gmail.com>
2018-01-12 15:55:46 +0530

    added tableView and poster images

---
+++ flix_path/MovieCell.swift
@@ -0,0 +1,30 @@
+//
+//  MovieCell.swift
+//  flix_path
+//
+//  Created by Bhavesh on 21/10/1939 Saka.
+//  Copyright © 1939 Bhavesh. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    @IBOutlet weak var overviewLabel: UILabel!
+    
+    @IBOutlet weak var posterImageView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
