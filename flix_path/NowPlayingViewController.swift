31f10b1e5de6cb621b8fe434dca93302d7f371c1
bhaveshc20 <bhaveshc789@gmail.com>
2018-01-12 21:13:00 +0530

    added pull to refresh

--- flix_path/NowPlayingViewController.swift
+++ flix_path/NowPlayingViewController.swift
@@ -14,28 +14,40 @@ class NowPlayingViewController: UIViewController, UITableViewDataSource {
     @IBOutlet weak var tableView: UITableView!
     
     var movies: [[String: Any]] = []
+    var refreshControl: UIRefreshControl!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+    
+        refreshControl = UIRefreshControl()
+        refreshControl.addTarget(self, action: #selector(NowPlayingViewController.didPullToRefresh(_:)), for: .valueChanged)
+        tableView.insertSubview(refreshControl, at: 0)
         
+        tableView.dataSource = self
+        fetchMovies()
+    
+    }
+    
+    @objc func didPullToRefresh(_ refreshControl: UIRefreshControl) {
+        fetchMovies()
+    }
+    func fetchMovies(){
+        let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed")!
         
-    tableView.dataSource = self
+        let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
         
-    let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed")!
+        let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
         
-    let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
-        
-    let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
-        
-    let task = session.dataTask(with: request) { (data, response, error) in
+        let task = session.dataTask(with: request) { (data, response, error) in
             // this will run when the network request returns
-        if let error = error {
-            print(error.localizedDescription)
-        } else if let data = data {
-            let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
-            let movies = dataDictionary["results"] as! [[String: Any]]
-            self.movies = movies
-            self.tableView.reloadData()
+            if let error = error {
+                print(error.localizedDescription)
+            } else if let data = data {
+                let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
+                let movies = dataDictionary["results"] as! [[String: Any]]
+                self.movies = movies
+                self.tableView.reloadData()
+                self.refreshControl.endRefreshing()
             }
         }
         task.resume()
