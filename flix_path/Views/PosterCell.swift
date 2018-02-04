9e97341ae128fca13a13afb9011488e344d82223
bhaveshc20 <bhaveshc789@gmail.com>
2018-02-03 17:50:38 -0800

    added collectionview

---
+++ flix_path/Views/PosterCell.swift
@@ -0,0 +1,14 @@
+//
+//  PosterCell.swift
+//  flix_path
+//
+//  Created by Bhavesh on 2/3/18.
+//  Copyright © 2018 Bhavesh. All rights reserved.
+//
+
+import UIKit
+
+class PosterCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterImageView: UIImageView!
+}
